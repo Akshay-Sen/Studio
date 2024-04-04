9042fcab05a0d769a45e35af8fb5d197b4664ab1
Akshay Sen <akshaysen6150@gmail.com>
2024-04-04 23:21:37 +0530

    header issues fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
 // import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import './index.scss'
-import Home from './components/home/Home';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Services from './components/services/Services';
-import Wedding from './components/wedding/Wedding';
-import Gallery from './components/gallery/Gallery';
-import Contact from './components/contact/Contact';
-// import About from './components/about/About.jsx';
-// import Services from './components/services/Services.jsx';
-// import Gallery from './components/gallery/Gallery.jsx';
-// import Contact from './components/contact/Contact.jsx';
-// import Layout from './components/Layout.jsx';
 
 
  
@@ -21,15 +9,7 @@ import Contact from './components/contact/Contact';
 function App() {
 
   return (
-    <>
-   {/* <Header />
-   <Home /> 
-   <Services />
-   <Wedding />
-   <Gallery />
-   <Contact />
-   <Footer /> */}
-    </>
+    <> </>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,31 +1,121 @@
-import React from 'react'
-import "./style.scss"
+// import React, { useState } from 'react'
+// import "./style.scss"
+// import { SlMenu } from "react-icons/sl";
+// // import { IoClose } from "react-icons/io5";
+// import logo from "../../assets/logo.png"
+// import { Link, NavLink } from 'react-router-dom';
+
+
+
+// function Header() {
+
+// // const [isMobile, setIsMobile] = useState(false);
+
+//     return (
+//         <div className='header'>
+//             <div className="logo">
+//                 <img src= {logo} alt="" />
+//             </div>
+//             <div className="navbar">
+//                 <nav>
+//                     <ul>
+//                         <li><Link to="">Home</Link></li>
+//                         <li><Link to="about">about</Link></li>
+//                         <li><Link to="services">Services</Link></li>
+//                         <li><Link to="gallery">Gallery</Link></li>
+//                         <li><Link to="contact">Contact</Link></li>
+//                     </ul>
+//                 </nav>
+//             </div>
+//             <div className="menu-bar">
+//                 <SlMenu />
+//             </div>
+//         </div>
+
+//         // <nav className='navbar'>
+//         //     <div className='logo'> Logo </div>
+//         //     <ul className= { isMobile ? "nav-links-mobile" : "nav-links"}>
+//         //         <li>
+//         //             <NavLink to="/" >Home</NavLink>
+//         //         </li>
+//         //         <li>
+//         //             <NavLink to="/about" >About</NavLink>
+//         //         </li>
+//         //         <li>
+//         //             <NavLink to="/services" >Services</NavLink>
+//         //         </li>
+//         //         <li>
+//         //             <NavLink to="/gallery" >Gallery</NavLink>
+//         //         </li>
+//         //         <li>
+//         //             <NavLink to="/contact" >Contact</NavLink>
+//         //         </li>
+//         //     </ul>
+
+//         //     <button className='mobile-menu-icon'
+//         //     onClick={()=> setIsMobile(!isMobile)}
+//         //     >
+//         //         {
+//         //             isMobile ? (
+//         //                 <IoClose />
+//         //             ) : (
+//         //                 <SlMenu />
+//         //             )
+//         //         }
+//         //     </button>
+
+//         // </nav>
+//     )
+// }
+
+// export default Header
+
+
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+// import { ReactComponent as Hamburger } from '../../assets/icons/hamburger.svg'
 import { SlMenu } from "react-icons/sl";
-import logo from "../../assets/logo.png"
-import { Link } from 'react-router-dom';
+// import { ReactComponent as Brand } from '../../assets/icons/logo.svg'
+import logo from "../../assets/logo1.png"
+import './style.scss'
 
+const Header = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
 
-function Header() {
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
   return (
-    <div className='header'>
+    <nav className="navbar">
+      <div className="container-1">
         <div className="logo">
-            <img src= {logo} alt="" />
+         <img src={logo} alt="" />
         </div>
-        <div className="navbar">
-            <nav>
-                <ul>
-                    <li><Link to="">Home</Link></li>
-                    <li><Link to="about">about</Link></li>
-                    <li><Link to="services">Services</Link></li>
-                    <li><Link to="gallery">Gallery</Link></li>
-                    <li><Link to="contact">Contact</Link></li>
-                </ul>
-            </nav>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <SlMenu />
         </div>
-        <div className="menu-bar">
-            <SlMenu />
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/services">Services</NavLink>
+            </li>
+            <li>
+              <NavLink to="/gallery">Gallery</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </ul>
         </div>
-    </div>
+      </div>
+    </nav>
   )
 }
 
