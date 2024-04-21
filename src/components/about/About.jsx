e6d6e721765c90ae6306eea2029bdbcd097d973c
Akshay Sen <akshaysen6150@gmail.com>
2024-04-21 16:24:46 +0530

    pragraph updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './style.scss'
 import photographer from '../../assets/photographer.jpg'
-// import studio from '../../assets/photographer1.jpg'
 
 function About() {
   return (
@@ -13,9 +12,9 @@ function About() {
         <div className='bio '>
           <div className="text right">
           <h2>About <span>Me</span> </h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium sint cum, quos voluptates quaerat dolorem saepe ad laudantium cupiditate dolor! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi necessitatibus est provident dignissimos, esse laborum fugiat non velit autem et labore sapiente veritatis quidem architecto cupiditate. </p>
+            <p>You need to use when you have to, such as when it’s raining. Or, when you just feel like it. Sometimes you wear it because you need to get the sun out of your eyes. Think about flash in the same way. Don’t use it all the time, but also don’t shy away from it. If it’s dark outside, use flash. If you know there’s time, and you have a good idea, and your clients will love it, then go for it. </p>
             <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates labore nemo et itaque. Dolorum numquam ad laboriosam commodi perspiciatis odio.
+            Photography is a way of feeling, of touching, of loving. What you have caught on film is captured forever… It remembers little things, long after you have forgotten everything.
             </span> <br />
             <button>Learn more </button>
            
